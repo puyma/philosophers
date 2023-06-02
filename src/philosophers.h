0decc1be3c38e9a3708c7257b1752af0a78104ae
puyma <mpuig-ma@student.42barcelona.com>
2023-06-02 19:06:51 +0200

    added unistd.h

--- src/philosophers.h
+++ src/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 19:18:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/01 19:19:38 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/02 19:01:33 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define PHILOSOPHERS_H
 
 # include <stdio.h> /* printf */
+# include <unistd.h> /* write */
 
 #endif /* philosophers.h */
