3c30e610030926c71d07f42668966c25d620c2e2
puyma <mpuig-ma@student.42barcelona.com>
2023-06-05 18:37:28 +0200

    mv s_data

--- src/philosophers.h
+++ src/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 19:18:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/05 18:08:30 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/05 18:26:20 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@
 # include <unistd.h> /* write */
 
 # define USAGE "Usage: n_philosophers tt_die tt_eat tt_sleep [n_eat_times]\n"
+
+typedef struct s_data
+{
+	int	n_philo;
+	int	tt_die;
+	int	tt_eat;
+	int	tt_sleep;
+	int	n_times_eat;
+}		t_data;
 
 int		str_isdigit(char *str);
 int		ft_putstr_fd(char *s, int fd);
