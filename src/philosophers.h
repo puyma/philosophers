9b19789060c5aaf502dbf55bc06423eae22f94d2
puyma <mpuig-ma@student.42barcelona.com>
2023-06-05 18:17:12 +0200

    usage macro, func defs

--- src/philosophers.h
+++ src/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 19:18:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/02 19:01:33 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/05 18:08:30 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define PHILOSOPHERS_H
 
 # include <stdio.h> /* printf */
+# include <stdlib.h> /* malloc, EXIT_MACROS */
 # include <unistd.h> /* write */
+
+# define USAGE "Usage: n_philosophers tt_die tt_eat tt_sleep [n_eat_times]\n"
+
+int		str_isdigit(char *str);
+int		ft_putstr_fd(char *s, int fd);
 
 #endif /* philosophers.h */
