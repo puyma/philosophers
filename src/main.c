5cbfb6d6b84c52fdc838c78785429041e49093c6
puyma <mpuig-ma@student.42barcelona.com>
2023-06-05 18:37:41 +0200

    check_args OK

--- src/main.c
+++ src/main.c
@@ -6,22 +6,14 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 19:17:11 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/05 18:08:22 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/05 18:36:06 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-typedef struct s_data
-{
-	int	n_philo;
-	int	tt_die;
-	int	tt_eat;
-	int	tt_sleep;
-	int	n_times_eat;
-}		t_data;
-
-int		check_args(int argc, char **argv, t_data *data);
+static int	check_args(int argc, char **argv, t_data *data);
+int			unamed_philosophers(t_data *data);
 
 int	main(int argc, char **argv)
 {
@@ -29,21 +21,38 @@ int	main(int argc, char **argv)
 
 	if (check_args(argc, argv, &data) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
+	unamed_philosophers(&data);
 	return (EXIT_SUCCESS);
 }
 
-int	check_args(int argc, char **argv, t_data *data)
+int	unamed_philosophers(t_data *data)
 {
 	int	i;
 
-	i = 1;
-	if (argc != 5 && argc != 6)
-		return (ft_putstr_fd(USAGE, STDERR_FILENO));
-	while (i < argc)
+	i = 0;
+	while (i < data->n_philo)
 	{
-		if (argv[i] == NULL || *argv[i] == '\0' || str_isdigit(argv[i]) != 0)
-			return (EXIT_FAILURE);
+		ft_putstr_fd("Hey\n", STDOUT_FILENO);
 		++i;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	check_args(int argc, char **argv, t_data *data)
+{
+	if (argc != 5 && argc != 6)
+	{
+		ft_putstr_fd(USAGE, STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	while (--argc)
+	{
+		if (argv[argc] == NULL || *argv[argc] == '\0'
+			|| str_isdigit(argv[argc]) != EXIT_SUCCESS)
+		{
+			ft_putstr_fd("Error: invalid argument(s)\n", STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
 	}
 	data->n_times_eat = -1;
 	data->n_philo = atoi(argv[1]);
