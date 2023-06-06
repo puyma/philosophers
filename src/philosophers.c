67066b6f826d58951c74b2a6235b6c597b6448b0
puyma <mpuig-ma@student.42barcelona.com>
2023-06-06 20:17:04 +0200

    norm

--- src/philosophers.c
+++ src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:25:43 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/06 20:06:52 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/06 20:16:54 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,16 @@
 int	init_philosophers(t_data *data)
 {
 	int		i;
-	int		spoons[2];
 	t_philo	*philo;
 
 	i = 0;
 	while (i < data->n_philo)
 	{
 		philo = data->philo[i];
-		spoons[0] = which_fork(i, data->n_philo, LEFT);
-		spoons[1] = which_fork(i, data->n_philo, RIGHT);
-		philo->spoon[0] = data->mutexes[spoons[0]];
-		philo->spoon[1] = data->mutexes[spoons[1]];
-		//printf("Philosopher %d will try to pick L: %d, R: %d\n",
-		//	i, spoons[0], spoons[1]);
+		philo->spoon[0] = data->mutexes[which_fork(i, data->n_philo, LEFT)];
+		philo->spoon[1] = data->mutexes[which_fork(i, data->n_philo, RIGHT)];
 		if (pthread_create(&philo->th, NULL, &routine, NULL) != 0)
-            perror("Failed to created thread");
+			perror("Failed to created thread");
 		++i;
 	}
 	i = 0;
@@ -38,10 +33,7 @@ int	init_philosophers(t_data *data)
 	{
 		philo = data->philo[i];
 		if (pthread_join(philo->th, NULL) != 0)
-		{
-			printf("i: %d\n", i);
-            perror("Failed to join thread");
-		}
+			perror("Failed to join thread");
 		++i;
 	}
 	return (EXIT_SUCCESS);
