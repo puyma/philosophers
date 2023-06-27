f5df634c882d594626e370e08b3ab58d8cd32f40
puyma <mpuig-ma@student.42barcelona.com>
2023-06-27 13:50:59 +0200

    reset main functions

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 18:47:35 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/26 17:16:55 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/27 13:50:22 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,55 +21,16 @@ void	*routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *) arg;
-	while (philo->is_alive == TRUE)
-	{
-		if (philo->spoon[0] == philo->spoon[1])
-		{ printf("same\n"); 
-			break ;}
-		p_eat(philo);
-		p_wait(philo, philo->tt_sleep, SLEEP);
-		p_wait(philo, philo->tt_think, THINK);
-	}
-	log_stuff(philo, DIE, 0);
 	return (NULL);
 }
 
 int	p_eat(t_philo *philo)
 {
-	struct timeval	*t;
-	time_t			time;
-
-	t = &philo->timestamp;
-	pthread_mutex_lock(philo->spoon[1]);
-	gettimeofday(t, NULL);
-	log_stuff(philo, FORK, t->tv_usec - philo->init_time->tv_usec);
-	printf("[%d] trying\n", philo->id + 1);
-	pthread_mutex_lock(philo->spoon[0]);
-	gettimeofday(t, NULL);
-	log_stuff(philo, FORK, t->tv_usec - philo->init_time->tv_usec);
-	gettimeofday(t, NULL);
-	log_stuff(philo, EAT, (t->tv_usec - philo->init_time->tv_usec));
-	while ((t->tv_usec - philo->last_meal->tv_usec) < philo->tt_eat)
-	{
-		gettimeofday(t, NULL);
-	}
-	gettimeofday(t, NULL);
-	gettimeofday(&philo->timestamp, NULL);
-	philo->last_meal = &philo->timestamp;
-	time = t->tv_usec - philo->last_meal->tv_usec;
-	pthread_mutex_unlock(philo->spoon[0]);
-	pthread_mutex_unlock(philo->spoon[1]);
 	return (0);
 }
 
 int	p_wait(t_philo *philo, time_t tt_time, char *action)
 {
-	struct timeval	t;
-
-	gettimeofday(&t, NULL);
-	log_stuff(philo, action, t.tv_usec - philo->init_time->tv_usec);
-	while ((t.tv_usec - philo->last_meal->tv_usec) < tt_time)
-		gettimeofday(&t, NULL);
 	return (0);
 }
 
